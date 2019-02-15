52240585d9148d5358296f193a12811875b64e91
Abrehet <Gualwediasfaha@gmail.com>
2019-02-15 11:25:04 -0500

    adding sub method

--- src/Cal.java
+++ src/Cal.java
@@ -1,11 +1,14 @@
 public class Cal {
 
         public static double add(double num1,double num2){
-return num1+num2;
 
+         double s= num1+num2;
+         return s;
 
     }
     public static double subtract(double num1, double num2){
             return num1-num2;
+
     }
+
 }
