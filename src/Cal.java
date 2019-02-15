c99962c3d3017b74d58d14b4b7e498d6f13eb8ce
Abrehet <Gualwediasfaha@gmail.com>
2019-02-15 17:10:47 -0500

    thehhh

--- src/Cal.java
+++ src/Cal.java
@@ -8,14 +8,13 @@ public class Cal {
 
     }
     public static double subtract(double num1, double num2){
-<<<<<<< HEAD
+
             double resu=num1 -num2;
             return resu;
-=======
-            double result=num1-num2;
-            return result;
+
+
             
->>>>>>> 98ea3a00dbded749c7d31aa358ba6d58d0f4d9f5
+
 
     }
 
