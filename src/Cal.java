98ea3a00dbded749c7d31aa358ba6d58d0f4d9f5
abrehetdawit <46763559+abrehetdawit@users.noreply.github.com>
2019-02-15 16:54:41 -0500

    Update Cal.java

--- src/Cal.java
+++ src/Cal.java
@@ -7,7 +7,9 @@ public class Cal {
 
     }
     public static double subtract(double num1, double num2){
-            return num1-num2;
+            double result=num1-num2;
+            return result;
+            
 
     }
 
