017d5986d45f8de8ae02f6c959a6cc610d0720f8
Abrehet <Gualwediasfaha@gmail.com>
2019-02-15 16:58:33 -0500

    the new com

--- src/Cal.java
+++ src/Cal.java
@@ -5,9 +5,11 @@ public class Cal {
          double s= num1+num2;
          return s;
 
+
     }
     public static double subtract(double num1, double num2){
-            return num1-num2;
+            double resu=num1 -num2;
+            return resu;
 
     }
 
